27a2a449e44beffff2bc7c9b8dbc9fc862854a65
Benjamin Nortier <bjnortier@gmail.com>
2010-06-07 10:27:31 +0100

    Added new user selected Added validation

--- src/event.erl
+++ src/event.erl
@@ -9,26 +9,72 @@ title() -> "dayfindr.com".
 layout() ->
     #container_12 { 
 	body=[
-	      #grid_10 {alpha=true, prefix=1, suffix=1, body=participants()},
+	      #grid_10 {alpha=true, prefix=1, suffix=1, body=participants_panel()},
 	      #grid_10 {prefix=1, suffix=1, omega=true, body=body()}
 	     ]}.
 
 
+
+participants_panel() ->
+    #panel {id=participantPanel,
+	    body=participant_body()}.
+
+participant_body() ->
+    participant_body(undefined).
+
+participant_body(SelectedName) ->
+    Participants = participants(),
+    wf:wire(addButton, newParticipant, 
+	    #validate { validators=[
+				    #is_required { text="Please add your name" },
+				    #custom { text="Please choose a unique name", function=fun validate_unique/2 }
+				   ]}),
+    [
+     case length(Participants) of
+	 0 -> [];
+	 _ -> #label { text="I've been here before:" }
+     end,
+     #radiogroup { body=
+		   %% Need to flatten the list for the correct radio grouping
+		   lists:flatten(
+		     lists:map(
+		       fun({Id, Name}) ->
+			       ElementId = list_to_atom("participat_" ++ integer_to_list(Id)),
+			       [
+				#radio { id=ElementId, text=Name, 
+					 value=io_lib:format("~p", [Id]), 
+					 checked=SelectedName =:= Name,
+					 postback={user_selected, Id, Name}}, 
+				#br{}
+			       ]
+		       end,
+		       Participants))},
+     #panel {body=
+	     [
+	      #label { text="I'm new:" },
+	      #textbox { id=newParticipant, text="" },
+	      #br{},
+	      #button { id=addButton, text="Ok", postback=add_user }
+	     ]}
+    ].
+
 participants() ->
-    Participants = [{0, "Benjamin"}, {1, "Annie"}, {2, "John"}],
-    #radiogroup { body=
-		  %% Need to flatten the list for the correct radio grouping
-		  lists:flatten(
-		    lists:map(
-		      fun({Id, Name}) ->
-			      ElementId = list_to_atom("participat_" ++ integer_to_list(Id)),
-			      [
-			       #radio { id=ElementId, text=Name, value=io_lib:format("~p", [Id]), checked=false, postback={user_selected, Id, Name}}, 
-			       #br{}
-			      ]
-		      end,
-		      Participants))}.
-		   
+    %% The participants are the keys in the bucket, but
+    %% disregarding any metadata values that start with "_"
+    Client = riak_client(),
+    {ok, Keys} = Client:list_keys(bucket()),
+    StringKeys = lists:map(fun binary_to_list/1, Keys),
+    Filtered = [X || X <- StringKeys, hd(X) /= $_],
+    to_participants(Filtered).
+
+to_participants(Keys) ->
+    to_participants(Keys, [], 1).
+
+to_participants([Hd|Rest], Participants, Index) ->
+    to_participants(Rest, [{Index, Hd}|Participants], Index + 1);
+to_participants([], Participants, _) ->
+    Participants.
+
 
 body() -> 
     wf:wire(#api { name=save }),
@@ -53,12 +99,16 @@ script() ->
         $('.day_link').addClass('hidden_link');
     });".
 	
-event(click) ->
-    wf:replace(button, #panel { 
-        body="You clicked the button!", 
-        actions=#effect { effect=highlight }
-    });
-event({user_selected, Id, Name}) ->
+event(add_user) ->
+    Name = wf:q(newParticipant),
+    save(riak_client(), {list_to_atom(Name), "[]"}),
+    wf:replace(participantPanel, participant_body(Name)),
+    wf:wire( #script{ script="
+      $('.day_link').removeClass('hidden_link');
+      wave.setViewerId('" ++ Name ++ "');
+    "}),
+    ok;
+event({user_selected, _Id, Name}) ->
     %% Show the links when a participant is defined
     wf:wire( #script{ script="
       $('.day_link').removeClass('hidden_link');
@@ -76,6 +126,13 @@ api_event(save, _, [Hash]) ->
 		      save(Client, KV)
 	      end,
 	      Hash).
+
+validate_unique(_Tag, Value) ->
+    case lists:keyfind(Value, 2, participants()) of
+	false -> true;
+	_ -> false
+    end.
+	     
 
 save(Client, {Key, Value}) ->
     RiakKey = list_to_binary(atom_to_list(Key)),
